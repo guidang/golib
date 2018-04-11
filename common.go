3736e6618d0bd5aa7d5ed5d782a90550f7844bcd
skiy <dev@skiy.net>
2018-04-11 14:34:09 +0800

    update substr

--- common.go
+++ common.go
@@ -104,3 +104,20 @@ func Long2ip(ipLong uint32) string {
 	ip := net.IP(ipByte)
 	return ip.String()
 }
+
+/**
+截取字符串
+*/
+func Substr(str string, start int, end int) string {
+	rs := []rune(str)
+	length := len(rs)
+
+	if start < 0 || start > length {
+		return ""
+	}
+
+	if end < 0 || end > length {
+		return ""
+	}
+	return string(rs[start:end])
+}
